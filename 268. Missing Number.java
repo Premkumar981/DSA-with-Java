4530617a8b02e9e6e8d29cab5db290a10af816fe
Suru Prem Kumar <118878059+Premkumar981@users.noreply.github.com>
2024-10-03 21:57:57 +0530

    Add files via upload

---
+++ 268. Missing Number.java
@@ -0,0 +1,14 @@
+class Solution {
+    public int missingNumber(int[] nums) {
+        int range = nums.length;
+        int actualSum = (range*(range+1))/2;
+        int currentSum = 0;
+
+        for(int i=0;i<nums.length;i++){
+            currentSum = currentSum + nums[i];
+        }
+        
+        int ans = actualSum - currentSum;
+        return ans;
+    }
+}
