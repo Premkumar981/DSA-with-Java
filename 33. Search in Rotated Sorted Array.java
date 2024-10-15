5b65818dac3edc38b331f24b355781cfe4956ef7
Suru Prem Kumar <118878059+Premkumar981@users.noreply.github.com>
2024-10-15 13:50:49 +0530

    Update 33. Search in Rotated Sorted Array.java

--- 33. Search in Rotated Sorted Array.java
+++ 33. Search in Rotated Sorted Array.java
@@ -15,7 +15,7 @@ class Solution {
                     start = mid + 1;
                 }
             }else{
-                if(target>nums[mid] && target<nums[end]){
+                if(target>nums[mid] && target<=nums[end]){
                     start = mid + 1;
                 }else{
                     end = mid - 1;
