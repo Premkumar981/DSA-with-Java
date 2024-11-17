31bf6ea6efc510d404b26a76880df1f47fceeb00
Suru Prem Kumar <118878059+Premkumar981@users.noreply.github.com>
2024-11-17 23:27:30 +0530

    Create 74. Search a 2D Matrix.java

---
+++ 74. Search a 2D Matrix.java
@@ -0,0 +1,18 @@
+class Solution {
+    public boolean searchMatrix(int[][] matrix, int target) {
+        int row = 0;
+        int col = matrix[0].length-1;
+
+        while(row<matrix.length && col>=0){
+            if(target == matrix[row][col]){
+                return true;
+            }else if(target < matrix[row][col]){
+                col--;
+            }else{
+                row++;
+            }
+        }
+        return false;
+    }
+}
+//premkumarsuru
