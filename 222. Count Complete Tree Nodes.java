3635b8a3964ac4bb83c06d70eaba70e1cd7c7a4e
Suru Prem Kumar <118878059+Premkumar981@users.noreply.github.com>
2024-12-05 18:23:11 +0530

    Create 222. Count Complete Tree Nodes.java

---
+++ 222. Count Complete Tree Nodes.java
@@ -0,0 +1,28 @@
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode() {}
+ *     TreeNode(int val) { this.val = val; }
+ *     TreeNode(int val, TreeNode left, TreeNode right) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ */
+class Solution {
+    public int countNodes(TreeNode root) {
+        if(root == null){
+            return 0;
+        }
+
+        int leftSide = countNodes(root.left);
+        int rightSide = countNodes(root.right);
+
+        return leftSide + rightSide + 1;
+    }
+}
+//premkumarsuru
