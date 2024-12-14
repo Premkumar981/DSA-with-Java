dca6352573070a39922be41974efc6ea87d134cc
Suru Prem Kumar <118878059+Premkumar981@users.noreply.github.com>
2024-12-14 15:41:49 +0530

    Create 199. Binary Tree Right Side View.java

---
+++ 199. Binary Tree Right Side View.java
@@ -0,0 +1,35 @@
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode() {}
+ *     TreeNode(int val) { this.val = val; }
+ *     TreeNode(int val, TreeNode left, TreeNode right) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ */
+class Solution {
+    public void helper(TreeNode root, List<Integer> ans, int level){
+        if(root == null){
+            return;
+        }
+
+        if(ans.size() == level){
+            ans.add(root.val);
+        }
+        helper(root.right, ans, level+1);
+        helper(root.left, ans, level+1);
+    }
+    public List<Integer> rightSideView(TreeNode root) {
+        List<Integer> ans = new ArrayList<>();
+        helper(root, ans, 0);
+
+        return ans;
+    }
+}
+//premkumarsuru
