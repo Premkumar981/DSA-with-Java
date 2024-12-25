a68ccfb895846a6907c2f8796489805edd7bd184
Suru Prem Kumar <118878059+Premkumar981@users.noreply.github.com>
2024-12-25 14:09:04 +0530

    Create 1038. Binary Search Tree to Greater Sum Tree.java

---
+++ 1038. Binary Search Tree to Greater Sum Tree.java
@@ -0,0 +1,36 @@
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode() {}
+ *     TreeNode(int val) { this.val = val; }
+ *     TreeNode(int val, TreeNode left, TreeNode right) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ */
+class Solution {
+    int sum = 0;
+    public void helper(TreeNode root){
+        if(root == null){
+            return;
+        }
+
+        helper(root.right);
+
+        sum += root.val;
+        root.val = sum;
+
+        helper(root.left);
+    }
+    public TreeNode bstToGst(TreeNode root) {
+        helper(root);
+
+        return root;
+    }
+}
+//premkumarsuru
